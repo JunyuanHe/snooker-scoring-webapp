293f36374ecaff98dc54fb479471d88a1a9bde36
Junyuan He <hejunyuan01@126.com>
2025-01-04 17:50:50 +0800

    moved balls to scene

--- src/components/modules/Player.js
+++ src/components/modules/Player.js
@@ -26,89 +26,38 @@ class ClickMe extends React.Component {
   }
 
 
-const Player = () => {
-    const [totalScore, setTotalScore] = useState(0);
-    const [breakScore, setBreakScore] = useState(0);
-    const photoDict = {
-        red: "/public/red.png",
-        yellow: "yellow.png",
-    };
-    const images = require.context('../../public', true);
-    const imageList = images.keys().map(image => images(image));
-    console.log(images);
-    console.log(imageList[1]);
+const Player = (props) => {
+    // const [totalScore, setTotalScore] = useState(0);
+    // const [breakScore, setBreakScore] = useState(0);
 
-    const addToTotal = () => {
-        setTotalScore(totalScore + breakScore);
-    };
+    // const addToTotal = () => {
+    //     setTotalScore(totalScore + breakScore);
+    // };
 
-    const clearTotal = () => {
-        setTotalScore(0);
-    }
+    // const clearTotal = () => {
+    //     setTotalScore(0);
+    // }
 
-    const clearBreak = () => {
-        setBreakScore(0);
-    };
+    // const clearBreak = () => {
+    //     setBreakScore(0);
+    // };
 
-    const addToBreak = (score) => {
-        setBreakScore(breakScore + score);
-    };
+    // const addToBreak = (score) => {
+    //     setBreakScore(breakScore + score);
+    // };
 
     return (
         <div>
             <div className="Player-score-container">
-                <div className="Player-score">
+                <div className={props.round==true? "Player-score-selected": "Player-score"}>
                     {/* <h3>Score</h3> */}
-                    <h1>{totalScore}</h1>
+                    <h1>{props.totalScore}</h1>
                 </div>
             </div>
             <div className="Player-score-container">
-                <div className="Player-score">
+                <div className={props.round==true? "Player-score-selected": "Player-score"}>
                     {/* <h3>Break</h3> */}
-                    <h2>Break: {breakScore}</h2>
-                </div>
-            </div>
-            <div className="Player-ball-container">
-                <div className="Player-all-balls grid-red">
-                    <div className="Player-red Player-ball effect3" onClick={() => { addToBreak(1); }}>
-                        Red
-                    </div>
-                </div>
-                
-                <div className="Player-all-balls grid">
-                    <div className="Player-yellow Player-ball effect3" onClick={() => { addToBreak(2); }}>
-                        Yellow
-                    </div>
-                    <div className="Player-green Player-ball effect3" onClick={() => { addToBreak(3); }}>
-                        Green
-                    </div>
-                    <div className="Player-brown Player-ball effect3" onClick={() => { addToBreak(4); }}>
-                        Brown
-                    </div>
-                    <div className="Player-blue Player-ball effect3" onClick={() => { addToBreak(5); }}>
-                        Blue
-                    </div>
-                    <div className="Player-pink Player-ball effect3" onClick={() => { addToBreak(6); }}>
-                        Pink
-                    </div>
-                    <div className="Player-black Player-ball effect3" onClick={() => { addToBreak(7); }}>
-                        Black
-                    </div>
-                </div>
-            </div>
-            <div className="Player-general-container">
-                <div className="Player-general effect1" onClick={() => { addToTotal(); clearBreak(); }}>
-                    <h4>Submit</h4>
-                </div>
-            </div>
-            <div className="Player-general-container">
-                <div className="Player-general effect2" onClick={() => { clearBreak(); }}>
-                    <h4>Reset Break</h4>
-                </div>
-            </div>
-            <div className="Player-general-container">
-                <div className="Player-general effect2" onClick={() => { clearBreak(); clearTotal(); }}>
-                    <h4>Clear All</h4>
+                    <h2>Break: {props.breakScore}</h2>
                 </div>
             </div>
         </div>
--- src/components/pages/Scene.js
+++ src/components/pages/Scene.js
@@ -6,18 +6,85 @@ import "./Scene.css";
 
 
 const Scene = () => {
-    // const [totalScore1, setTotalScore1] = useState(0);
+    const [totalScore1, setTotalScore1] = useState(0);
+    const [breakScore1, setBreakScore1] = useState(0);
+    const [totalScore2, setTotalScore2] = useState(0);
+    const [breakScore2, setBreakScore2] = useState(0);
+    const [round, setRound] = useState(1);
+
+    const addToTotal = () => {
+        round == 1? setTotalScore1(totalScore1 + breakScore1): setTotalScore2(totalScore2 + breakScore2);
+    };
+
+    const clearTotal = () => {
+        setTotalScore1(0);
+        setTotalScore2(0);
+    }
+
+    const clearBreak = () => {
+        round == 1? setBreakScore1(0): setBreakScore2(0);
+    };
+
+    const addToBreak = (score) => {
+        round == 1? setBreakScore1(breakScore1 + score): setBreakScore2(breakScore2 + score);
+    };
 
     return (
+        <>
         <div className="u-flex">
             <div className="Player-container u-textCenter">
-                <Player />
+                <div className="effect3" onClick={() => setRound(1)}>
+                    <Player totalScore={totalScore1} breakScore={breakScore1} round={round==1}/>
+                </div>
             </div>
             <div className="Player-container u-textCenter">
-                <Player />
+                <div className="effect3" onClick={() => setRound(2)}>
+                    <Player totalScore={totalScore2} breakScore={breakScore2} round={round!=1}/>
+                </div>
             </div>
         </div>
+        <div className="Player-ball-container">
+            <div className="Player-all-balls">
+                <div className="Player-red Player-ball effect3" onClick={() => { addToBreak(1); }}>
+                    Red
+                </div>
+                <div className="grid">
+                <div className="Player-yellow Player-ball effect3" onClick={() => { addToBreak(2); }}>
+                    Yellow
+                </div>
+                <div className="Player-green Player-ball effect3" onClick={() => { addToBreak(3); }}>
+                    Green
+                </div>
+                <div className="Player-brown Player-ball effect3" onClick={() => { addToBreak(4); }}>
+                    Brown
+                </div>
+                <div className="Player-blue Player-ball effect3" onClick={() => { addToBreak(5); }}>
+                    Blue
+                </div>
+                <div className="Player-pink Player-ball effect3" onClick={() => { addToBreak(6); }}>
+                    Pink
+                </div>
+                <div className="Player-black Player-ball effect3" onClick={() => { addToBreak(7); }}>
+                    Black
+                </div>
+                </div>
+            </div>
+        </div>
+        <div className="Player-general-container">
+            <button className="Player-general effect1" onClick={() => { addToTotal(); clearBreak(); }}>
+                <h4>Submit</h4>
+            </button>
+            <button className="Player-general effect2" onClick={() => { clearBreak(); }}>
+                <h4>Reset Break</h4>
+            </button>
+            <button className="Player-general effect2" onClick={() => { clearBreak(); clearTotal(); }}>
+                <h4>Clear All</h4>
+            </button>
+        </div>
+        </>
     );
+
+
 };
 
 export default Scene;
